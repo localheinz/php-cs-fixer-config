f4bcfda44f54de2aa9c3b16eb7c5591511f5890f
Andreas Möller <am@localheinz.com>
2017-04-21 19:28:33 +0200

    Enhancement: Enable list_syntax fixer

--- test/Unit/RuleSet/Php71Test.php
+++ test/Unit/RuleSet/Php71Test.php
@@ -64,7 +64,9 @@ final class Php71Test extends AbstractRuleSetTestCase
                 'use_yoda_style' => true,
             ],
             'linebreak_after_opening_tag' => true,
-            'list_syntax' => false,
+            'list_syntax' => [
+                'syntax' => 'short',
+            ],
             'lowercase_cast' => true,
             'magic_constant_casing' => true,
             'mb_str_functions' => true,
