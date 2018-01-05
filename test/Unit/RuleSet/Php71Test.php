dd6b5696fdd92ed6b11e68f58576e8392f4d1f23
Andreas Möller <am@localheinz.com>
2018-01-05 16:56:09 +0100

    Enhancement: Add more tokens to no_extra_consecutive_blank_lines fixer

--- test/Unit/RuleSet/Php71Test.php
+++ test/Unit/RuleSet/Php71Test.php
@@ -115,12 +115,15 @@ final class Php71Test extends AbstractRuleSetTestCase
         'no_extra_consecutive_blank_lines' => [
             'tokens' => [
                 'break',
+                'case',
                 'continue',
                 'curly_brace_block',
+                'default',
                 'extra',
                 'parenthesis_brace_block',
                 'return',
                 'square_brace_block',
+                'switch',
                 'throw',
                 'use',
                 'use_trait',
