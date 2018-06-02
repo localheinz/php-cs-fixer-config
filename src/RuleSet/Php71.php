a051291fa2ece85030a1a34d8500eff7b02c0db0
Andreas Möller <am@localheinz.com>
2018-06-03 00:01:51 +0200

    Enhancement: Use always_move option for yoda_style fixer

--- src/RuleSet/Php71.php
+++ src/RuleSet/Php71.php
@@ -289,6 +289,7 @@ final class Php71 extends AbstractRuleSet
         'void_return' => false,
         'whitespace_after_comma_in_array' => true,
         'yoda_style' => [
+            'always_move_variable' => true,
             'equal' => true,
             'identical' => true,
             'less_and_greater' => true,
