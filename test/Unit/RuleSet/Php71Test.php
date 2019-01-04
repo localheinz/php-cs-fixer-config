b01d210471af67a3d120bd8be5ba8b53ffcc13e4
Andreas Möller <am@localheinz.com>
2019-01-04 22:38:32 +0100

    Enhancement: Enable no_unset_cast fixer

--- test/Unit/RuleSet/Php71Test.php
+++ test/Unit/RuleSet/Php71Test.php
@@ -193,7 +193,7 @@ final class Php71Test extends AbstractRuleSetTestCase
         'no_unneeded_curly_braces' => true,
         'no_unneeded_final_method' => true,
         'no_unreachable_default_argument_value' => true,
-        'no_unset_cast' => false,
+        'no_unset_cast' => true,
         'no_unset_on_property' => true,
         'no_unused_imports' => true,
         'no_useless_else' => true,
