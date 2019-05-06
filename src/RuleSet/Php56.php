0d148cc25884f90f464fc57f22b2263bc320d4ca
Andreas Möller <am@localheinz.com>
2019-05-06 21:22:53 +0200

    Enhancement: Enable php_unit_mock_short_will_return fixer

--- src/RuleSet/Php56.php
+++ src/RuleSet/Php56.php
@@ -221,7 +221,7 @@ final class Php56 extends AbstractRuleSet
         ],
         'php_unit_method_casing' => true,
         'php_unit_mock' => true,
-        'php_unit_mock_short_will_return' => false,
+        'php_unit_mock_short_will_return' => true,
         'php_unit_namespaced' => [
             'target' => '5.7',
         ],
